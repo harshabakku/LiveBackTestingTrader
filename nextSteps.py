51171f40171c9f5de0cc0db2fbb3fb18d2d0a1ec
harshafyniti <sriharsha.bakku@fyniti.com>
2020-04-13 03:04:49 +0530

    81 percent per year on wazirx USDT INR market

---
+++ nextSteps.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: harsha
+
+
+
+get 30 min data(last 1 year), exhaustive test using sma crossover to identify the peak profit params //done
+81 percent per year on wazir USDT INR market so far.
+
+360 percent is the goal. position sizing trying to catch more signals, long, short at the same time, along with leverage.(vwap, volume, RSI divergence is key)
+
+find signals and place multiple buy orders wait for their take profit close.
+
+test with wazirx data as well.
+
+"""
+
